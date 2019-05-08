6db5386d183fa2084245c8763003cd1bb9001492
charith <charith.v@jgsw.global>
2019-05-08 11:56:30 +0530

    Update version 0.1.0

--- app/src/main/java/com/info/charith/paycardview/MainActivity.java
+++ app/src/main/java/com/info/charith/paycardview/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
